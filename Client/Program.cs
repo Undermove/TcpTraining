ad757a55faec1205857947a5cb24261dffe53279
Dmitry Afonchenko <undermove@yandex.ru>
2020-11-15 13:35:19 +0300

    Prepare for decomposition

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,10 +10,19 @@ namespace Client
             Connect("127.0.0.1", "String message");
         }
 
+        // Functions:
+        // Connect()
+        // SendMessage()
+        // StartListening()
+        // + OnMessageReceived
+        // StopListening()
+        // Disconnect()
+
         static void Connect(String server, String message)
         {
             try
             {
+                #region Connect
                 // Create a TcpClient.
                 // Note, for this client to work you need to have a TcpServer
                 // connected to the same address as specified by the server, port
@@ -21,21 +30,23 @@ namespace Client
                 Int32 port = 13000;
                 TcpClient client = new TcpClient(server, port);
 
-                // Translate the passed message into ASCII and store it as a Byte array.
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
                 // Get a client stream for reading and writing.
                 //  Stream stream = client.GetStream();
 
                 NetworkStream stream = client.GetStream();
+                #endregion Connect
+
+                #region SendMessage
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
 
                 // Send the message to the connected TcpServer.
                 stream.Write(data, 0, data.Length);
 
                 Console.WriteLine("Sent: {0}", message);
+                #endregion SendMessage
 
                 // Receive the TcpServer.response.
-
                 // Buffer to store the response bytes.
                 data = new Byte[256];
 
