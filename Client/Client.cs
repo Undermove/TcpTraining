ff485874e7c5c6c05b33260ce6bda26e4a1bc640
Dmitry Afonchenko <undermove@yandex.ru>
2020-11-15 14:05:37 +0300

    Add client to main

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Net.Sockets;
 
-namespace CleintClassNamespace
+namespace ClientClassNamespace
 {
     public class ClientClass 
     {
-        private string _serverAddress;
+        private readonly string _serverAddress;
+        private readonly int _port;
+
+        public ClientClass(string serverAddress, int port)
+        {
+            _serverAddress = serverAddress;
+            _port = port;
+        }
 
         public void Connect()
         {
-            Int32 port = 13000;
-            TcpClient client = new TcpClient(_serverAddress, port);
+            TcpClient client = new TcpClient(_serverAddress, _port);
             NetworkStream stream = client.GetStream();
         }
     }
