68bf6e853996b7085f95ea9ce437b0ab579fbee8
d.afonchenko <d.afonchenko@dodopizza.com>
2020-12-06 14:22:14 +0300

    add start

--- Listener/Listener.cs
+++ Listener/Listener.cs
@@ -1,10 +1,17 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace ListenerNamespace
 {
     public class Listener
     {
         public void Start()
         {
-            
+            TcpListener server = null;
+            int port = 13000;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            server = new TcpListener(localAddr, port);
+            server.Start();
         }
     }
 }
